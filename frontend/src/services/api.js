27ff49648a18224b5278f6dfb1690bf031afb24c
unknown <legend.varz@gmail.com>
2025-11-05 00:22:55 +0530

    vercel

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 // The API's base URL, pointing to your backend
-const API_URL = 'http://localhost:5000/api';
+const API_URL = '/api';
 
 const api = axios.create({
   baseURL: API_URL,
